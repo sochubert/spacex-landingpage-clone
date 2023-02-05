e63c6888b2cdac0275614169ae6db697cc8ffbe6
sochubert <sochubert@gmail.com>
2023-02-05 12:24:18 +0900

    Dark overlay & Remove scroll on menu opened

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
 
 btn.addEventListener("click", navToggle);
 
 function navToggle() {
   btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+  document.body.classList.toggle("stop-scrolling");
 }
