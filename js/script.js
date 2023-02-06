c679a93ff861b9534401812c068d0ac02af0dc19
sochubert <sochubert@gmail.com>
2023-02-06 23:23:26 +0900

    Stat counter JavaScript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,56 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
 
 btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
 
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute("data-target");
+      // Get current counter value
+      const c = +counter.innerText;
+
+      // Increment counter
+      const increment = target / 100;
+
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 15);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
+function resizeTo() {
+  counters.forEach((counter) => (counter.innerHTML = "0"));
 }
